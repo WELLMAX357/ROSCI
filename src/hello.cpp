bc88eccece5d4f268992f42d45878688b8793080
WELLMAX357 <WELLMAX357@users.noreply.github.com>
2022-12-27 09:16:10 +0000

    :art: Apply clang-format

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,18 +1,13 @@
 #include "rclcpp/rclcpp.hpp"
 
-
-int     main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 
+  rclcpp::init(argc, argv);
 
-     rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("hello");
+  RCLCPP_INFO(node->get_logger(), "Hello, ROS2 world!");
 
-
-     auto node = rclcpp::Node::make_shared("hello");
-      RCLCPP_INFO(node->get_logger(), "Hello, ROS2 world!");
-
-
-
-    rclcpp::shutdown();
-return 0;
+  rclcpp::shutdown();
+  return 0;
 }
